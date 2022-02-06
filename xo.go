cf8c58b7bba831fda5957665e7ad2e525264734c
Nikita <lomello@yandex.ru>
2022-02-06 16:24:30 +0300

    xo lesson

--- xo.go
+++ xo.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var token string
+
+	fmt.Println("Введите ваше имя")
+	fmt.Scanln(&name)
+
+	fmt.Println("Крестик или нолик?Введите X или O.")
+	fmt.Scanln(&token)
+
+	fmt.Println(name, token)
+}
